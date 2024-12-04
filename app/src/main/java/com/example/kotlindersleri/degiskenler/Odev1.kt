004bb3f9b4eed2c59af45ad45f806839fdd9f306
emono469 <metro.1997@hotmail.com>
2024-12-04 03:36:37 +0300

    FMSS Bilişim Android Development Bootcamp with Jetpack Ödev 1

---
+++ app/src/main/java/com/example/kotlindersleri/degiskenler/Odev1.kt
@@ -0,0 +1,102 @@
+package com.example.kotlindersleri.degiskenler
+
+fun main() {
+
+    val ilce = "Beşiktaş"
+    val kita = "Asya"
+    val faks = "0 (312) 525 00 77"
+    val postaKod = "34225"
+    val inst = "instagram.com/michaeljackson"
+    val bolum = "Üretimhane"
+    val urunMiktar = 50
+    val musteriSoyad = "Öztürk"
+    val odemeMiktar = "25000 TL"
+    val dogumTarih = "29.10.1923"
+    val borc = "20000 TL"
+    val medeniHal = "Bekar"
+    val videoYorum = "\"Harika bir video!\""
+    val odemeSaati = "13:35"
+    val eftMiktar = "5000 TL"
+    val satilanMiktar = 200
+    val telefonModel = "Samsung Galaxy S24"
+    val dergiAd = "FourFourTwo"
+    val basimTarihi = "04.12.2024"
+    val zamMiktari = "%35"
+    val daireSayisi = 25
+    val enlem = "41°06'32.0\"N"
+    val boylam = "29°03'03.6\"E"
+    val yemekAd = "Sütlü Nuriye"
+    val urunFiyat = "100 TL"
+    val sirket = "Google"
+    val videoAd = "\"DÜNYANIN EN SAĞLAM SANDALYESİ!\""
+    val muzikSuresi = "4:13"
+    val mekanPuan = "4,2/5"
+    val dosyaAd = "index.html"
+    val resimFormat = ".jpeg"
+    val renk = "Siyah"
+    val renkKod = "#FFFFFF"
+    val bilgisayarModel = "Casper Nirnava X600"
+    val ekranBoyut = "17,3\""
+    val kullanimSuresi = "30 Gün"
+    val basinc = "1017,4 hPa"
+    val etkinlikGun = "Çarşamba"
+    val odemeGunu = "Perşembe"
+    val yolculukCikisTarih = "01.01.2025"
+    val mahalleAd = "Çengelköy"
+    val otobusHat = "15C"
+    val kullanilanDakika = 14
+    val kargoTakipNo = 120024874261
+    val kuponSuresi = 7
+    val kuponKod = "RX28HLV"
+    val faturaTarih = "02.12.2024"
+
+
+    println("İlçe                  : $ilce")
+    println("Kıta                  : $kita")
+    println("Faks                  : $faks")
+    println("Posta Kodu            : $postaKod")
+    println("Instagram             : $inst")
+    println("Çalışılan Bölüm       : $bolum")
+    println("Ürün Miktarı          : $urunMiktar")
+    println("Müşteri Soyadı        : $musteriSoyad")
+    println("Ödeme Miktarı         : $odemeMiktar")
+    println("Doğum Tarihi          : $dogumTarih")
+    println("Borç                  : $borc")
+    println("Medeni Hal            : $medeniHal")
+    println("Video Yorumu          : $videoYorum")
+    println("Ödeme Saati           : $odemeSaati")
+    println("EFT Miktarı           : $eftMiktar")
+    println("Satılan Miktar        : $satilanMiktar")
+    println("Telefon Modeli        : $telefonModel")
+    println("Dergi Adı             : $dergiAd")
+    println("Basım Tarihi          : $basimTarihi")
+    println("Zam Miktarı           : $zamMiktari")
+    println("Daire Sayısı          : $daireSayisi")
+    println("Enlem                 : $enlem")
+    println("Boylam                : $boylam")
+    println("Yemek Adı             : $yemekAd")
+    println("Ürün Fiyatı           : $urunFiyat")
+    println("Şirket                : $sirket")
+    println("Video Adı             : $videoAd")
+    println("Müzik Süresi          : $muzikSuresi")
+    println("Mekan Puanı           : $mekanPuan")
+    println("Dosya Adı             : $dosyaAd")
+    println("Resim Formatı         : $resimFormat")
+    println("Renk                  : $renk")
+    println("Renk Kodu             : $renkKod")
+    println("Bilgisayar Modeli     : $bilgisayarModel")
+    println("Ekran Boyutu          : $ekranBoyut")
+    println("Kullanım Süresi       : $kullanimSuresi")
+    println("Basınç                : $basinc")
+    println("Etkinlik Günü         : $etkinlikGun")
+    println("Ödeme Günü            : $odemeGunu")
+    println("Yolculuk Çıkış Tarihi : $yolculukCikisTarih")
+    println("Mahalle Adı           : $mahalleAd")
+    println("Otobüs Hattı          : $otobusHat")
+    println("Kullanılan Dakika     : $kullanilanDakika")
+    println("Kargo Takip No        : $kargoTakipNo")
+    println("Kupon Süresi          : $kuponSuresi Gün")
+    println("Kupon Kodu            : $kuponKod")
+    println("Fatura Tarihi         : $faturaTarih")
+
+}
